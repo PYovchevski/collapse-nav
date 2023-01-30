cab69d842f856ce5aec9a807d60f35aeb96a3cdb
Petko Yovchevski <pyovchevski@gmail.com>
2023-01-30 09:27:38 +0200

    updates

--- dist/collapseNav.js
+++ dist/collapseNav.js
@@ -1,6 +1,6 @@
 /*
 
- * CollapseNav.js - v1.0
+ * CollapseNav.js - v1.1
  * jQuery Responsive Navigation
  * MIT License
  * by Petko Yovchevski
@@ -12,185 +12,197 @@
 
  */
 
-function collapseNav(selector, config) {
-    var configuration = config;
-    var navigation = selector;
-    var original_navigation = $(selector).html();
+function initCollapseNav(selector, config, callback) {
+    const navigation = selector;
 
-    $(navigation).addClass('collapseNav-not-initialized');
+    navigation.classList.remove('collapseNav-not-initialized');
+    navigation.classList.add('collapseNav-initialized')
 
-    function init(selector, config) {
-        var navigation = selector;
-
-        $(navigation).removeClass('collapseNav-not-initialized');
-        $(navigation).addClass('collapseNav-initialized')
-
-        var responsive = config.responsive;
-        if (!responsive) {
-            responsive = 1;
-        }
-
-        var mobile_break = config.mobile_break;
-        if (!mobile_break) {
-            mobile_break = 992;
-        }
-
-        var li_class = config.li_class;
-        if (!li_class && li_class != '') {
-            li_class = 'dropdown';
-        }
-
-        var li_a_class = config.li_a_class;
-        if (!li_a_class && li_a_class != '') {
-            li_a_class = 'dropdown-toggle';
-        }
-
-        var li_ul_class = config.li_ul_class;
-        if (!li_ul_class && li_ul_class != '') {
-            li_ul_class = 'dropdown-menu';
-        }
-
-        var more_text = config.more_text;
-        if (!more_text) {
-            more_text = 'More';
-        }
-
-        var caret = config.caret;
-        if (!caret && caret != '') {
-            caret = '<span class="caret"></span>';
-        }
-
-        var ul_width = $(navigation).outerWidth();
-        var li_width;
-        var possible_buttons;
-        var li_count;
-
-        //console.log('ul: ' + ul_width);
-
-        /*---------------------------------------
-         --- Check base buttons to navigation  ---
-         --------------------------------------*/
-        li_width = 0;
-        possible_buttons = 0;
-        li_count = 0;
-
-        $(navigation).children("li").each(function (i) {
-            li_count = li_count + 1;
-            li_width = li_width + $(this).outerWidth(true);
-
-            //console.log(li_count + ' li ' + $(this).text() + ': ' + li_width);
-
-            if (ul_width >= li_width) {
-                possible_buttons = possible_buttons + 1;
-            }
-        });
-
-        // The navigation does not need a More menu, then stop the script
-        if (li_count <= possible_buttons) {
-            return;
-        }
-
-        //console.log('Possible buttons: ' + possible_buttons);
-
-
-        /*-------------------------------------------
-         --- Check the more buttons to navigation ---
-         ------------------------------------------*/
-        li_width = 0;
-        possible_buttons = 0;
-        li_count = 0;
-
-        //The More Button Width
-        $(navigation).children().first().clone().appendTo(navigation);
-        $(navigation).children().last().find('a').html(more_text + ' ' + caret).css({'vissibility': 'hidden'});
-        var the_more_button_width = $(navigation).children().last();
-
-        li_count = li_count + 1;
-        li_width = li_width + the_more_button_width.outerWidth(true);
-        //console.log(li_count + ' li More: ' + li_width);
-        the_more_button_width.remove();
-
-        $(navigation).children("li").each(function (i) {
-            li_count = li_count + 1;
-            li_width = li_width + $(this).outerWidth(true);
-
-            //console.log(li_count + ' li ' + $(this).text() + ': ' + li_width);
-
-            if (ul_width >= li_width) {
-                possible_buttons = possible_buttons + 1;
-            }
-        });
-
-        //console.log('Possible buttons: ' + (possible_buttons + 1) + ' + More button');
-
-        /*------------------
-         --- Some checks ---
-         -----------------*/
-
-        if (responsive == 1) {
-            number_of_buttons = possible_buttons;
-        } else {
-            var number_of_buttons = config.number_of_buttons;
-            if (!number_of_buttons) {
-                number_of_buttons = 4;
-            }
-        }
-        //console.log('Number of buttons: ' + number_of_buttons);
-
-        if ($(window).width() < mobile_break) {
-            return;
-        }
-
-        /*----------------------------------------
-         --- Convert the navigation to the new ---
-         ----------------------------------------*/
-        var btn_n = 0;
-        var ul = '<ul class="' + li_ul_class + '">'
-        $(navigation).children("li").each(function (i) {
-            btn_n = btn_n + 1;
-
-            if (btn_n > number_of_buttons) {
-                //console.log(btn_n + ' > ' + number_of_buttons);
-
-                ul += this.outerHTML;
-            }
-        })
-        ul += '</ul>';
-
-        number_of_buttons = number_of_buttons - 1;
-        $(navigation).children("li:gt(" + number_of_buttons + ")").remove();
-        $(navigation).append('<li class="' + li_class + '"><a href="javascript:;" class="' + li_a_class + '" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' + more_text + caret + '</a>' + ul + '</li>')
+    const defaults = {
+        debug: false,
+        responsive: 1, //Automatically count the possible buttons in the navigation
+        number_of_buttons: 4, //Allowable number of buttons in the navigation. Works only if 'responsive' = 0
+        more_text: 'More', //The text on the Drop Down Button
+        mobile_break: 992, //With this resolution and higher the script will be init
+        li_class: 'dropdown',
+        li_attrs: 'data-custom-attr="attr value"',
+        li_a_class: 'dropdown-toggle',
+        li_ul_class: 'dropdown-menu',
+        caret: '<span class="caret"></span>', //Element append immediately after the More text,
+        bootstrap_version: false
     }
 
-    $(window).on('load', function () {
-        if ($(window).width() >= configuration.mobile_break) {
-            $(navigation).html(original_navigation);
+    const configuration = Object.assign(defaults, config);
+
+    let ul_width = navigation.offsetWidth;
+    let li_width = 0;
+    let possible_buttons = 0;
+    let li_count = 0;
+
+    if (configuration.debug) {
+        console.log('ul: ' + ul_width);
+    }
+
+    /*---------------------------------------
+     --- Check base buttons to navigation  ---
+     --------------------------------------*/
+    // iterate over all child nodes
+    Array.from(navigation.children).forEach(li => {
+        li_count = li_count + 1;
+        li_width = li_width + li.offsetWidth;
+
+        if (ul_width >= li_width) {
+            possible_buttons = possible_buttons + 1;
         }
-        init(navigation, configuration);
-        // init(selector, config);
     });
 
-    $(window).on('resize collapseNavReInit', function () {
-        if ($(window).width() >= configuration.mobile_break) {
-            $(navigation).html(original_navigation);
+    // The navigation does not need a More menu, then stop the script
+    if (configuration.debug) {
+        console.log('li_count: ' + li_count);
+        console.log('possible_buttons: ' + possible_buttons);
+    }
+    if (li_count <= possible_buttons) {
+        return;
+    }
+
+    if (configuration.debug) {
+        console.log('Possible buttons: ' + possible_buttons);
+    }
+
+    /*-------------------------------------------
+     --- Check the more buttons to navigation ---
+     ------------------------------------------*/
+    li_width = 0;
+    possible_buttons = 0;
+    li_count = 0;
+
+    //The More Button Width
+
+    const newChild = navigation.firstElementChild.cloneNode(true);
+    navigation.appendChild(newChild);
+
+    navigation.lastElementChild.querySelector('a').innerHTML = configuration.more_text + ' ' + configuration.caret;
+    navigation.lastElementChild.querySelector('a').style.visibility = 'hidden';
+
+    const the_more_button_width = navigation.lastElementChild;
+
+    li_count = li_count + 1;
+    li_width = li_width + the_more_button_width.offsetWidth;
+    if (configuration.debug) {
+        console.log(li_count + ' li More: ' + li_width);
+    }
+    the_more_button_width.remove();
+
+    // iterate over all child nodes
+    Array.from(navigation.children).forEach(li => {
+        li_count = li_count + 1;
+        li_width = li_width + li.offsetWidth;
+
+        if (configuration.debug) {
+            console.log(li_count + ' li ' + $(this).text() + ': ' + li_width);
         }
-        init(navigation, configuration);
+
+        if (ul_width >= li_width) {
+            possible_buttons = possible_buttons + 1;
+        }
+    });
+
+    if (configuration.debug) {
+        console.log('Possible buttons: ' + (possible_buttons + 1) + ' + More button');
+    }
+
+    /*------------------
+     --- Some checks ---
+     -----------------*/
+    let number_of_buttons;
+
+    if (configuration.responsive == 1) {
+        number_of_buttons = possible_buttons;
+    } else {
+        number_of_buttons = configuration.number_of_buttons;
+        if (!number_of_buttons) {
+            number_of_buttons = 4;
+        }
+    }
+
+    if (configuration.debug) {
+        console.log('Number of buttons: ' + number_of_buttons);
+    }
+
+    if (window.innerWidth < configuration.mobile_break) {
+        return;
+    }
+
+    /*----------------------------------------
+     --- Convert the navigation to the new ---
+     ----------------------------------------*/
+    let btn_n = 0;
+    let ul = '<ul class="' + configuration.li_ul_class + '">'
+    // iterate over all child nodes
+    Array.from(navigation.children).forEach(li => {
+        btn_n = btn_n + 1;
+
+        if (btn_n > number_of_buttons) {
+            if (configuration.debug) {
+                console.log(btn_n + ' > ' + number_of_buttons);
+            }
+
+            ul += li.outerHTML;
+        }
+    });
+
+    ul += '</ul>';
+
+    number_of_buttons = number_of_buttons - 1;
+
+    $(navigation).children("li:gt(" + number_of_buttons + ")").remove();
+
+    const menuExpand = document.createElement('div');
+    if (configuration.bootstrap_version) {
+        if (configuration.bootstrap_version > 3) {
+            menuExpand.innerHTML = '<li class="' + configuration.li_class + '" ' + configuration.li_attrs + '><a href="javascript:;" class="' + configuration.li_a_class + '" data-bs-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' + configuration.more_text + configuration.caret + '</a>' + ul + '</li>';
+        } else {
+            menuExpand.innerHTML = '<li class="' + configuration.li_class + '" ' + configuration.li_attrs + '><a href="javascript:;" class="' + configuration.li_a_class + '" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' + configuration.more_text + configuration.caret + '</a>' + ul + '</li>';
+        }
+    } else {
+        menuExpand.innerHTML = '<li class="' + configuration.li_class + '" ' + configuration.li_attrs + '><a href="javascript:;" class="' + configuration.li_a_class + '" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">' + configuration.more_text + configuration.caret + '</a>' + ul + '</li>';
+    }
+
+    navigation.appendChild(menuExpand.firstChild);
+
+    if (callback) {
+        callback();
+    }
+}
+
+function collapseNav(selector, config, callback = false) {
+    const configuration = config;
+    const navigation = selector;
+    const original_navigation = {'outer': selector.outerHTML, 'inner': selector.innerHTML};
+
+    navigation.classList.add('collapseNav-not-initialized');
+
+    $(window).on('load resize collapseNavReInit', function () {
+        if (window.innerWidth >= configuration.mobile_break) {
+            $(navigation).html(original_navigation.inner);
+        }
+        initCollapseNav(navigation, configuration, callback);
     });
 
     window.addEventListener("orientationchange", function () {
-        if ($(window).width() >= configuration.mobile_break) {
-            $(navigation).html(original_navigation);
+        if (window.innerWidth >= configuration.mobile_break) {
+            $(navigation).html(original_navigation.inner);
         }
-        init(navigation, configuration);
+        initCollapseNav(navigation, configuration, callback);
     }, false);
 
-    init(navigation, configuration);
+    initCollapseNav(navigation, configuration, callback);
 }
 
-$.fn.collapseNav = function (config) {
+$.fn.collapseNav = function (config, callback = false) {
     config = config || {}
 
-    var defaults = {
+    const defaults = {
         responsive: 1, //Automatically count the possible buttons in the navigation
         number_of_buttons: 4, //Allowable number of buttons in the navigation. Works only if 'responsive' = 0
         more_text: 'More', //The text on the Drop Down Button
@@ -201,11 +213,11 @@ $.fn.collapseNav = function (config) {
         caret: '<span class="caret"></span>' //Element append immediately after the More text
     }
 
-    var settings = $.extend({}, defaults, config)
+    const settings = $.extend({}, defaults, config)
 
     return this.each(function () {
         setTimeout(function (scope) {
-            collapseNav(scope, settings);
+            collapseNav(scope, settings, callback);
         }, 700, this);
     })
 }
